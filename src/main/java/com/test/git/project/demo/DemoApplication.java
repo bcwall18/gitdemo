6879f79177265b4a7ddcac80f001dd149c650e30
Blair Wallace <blairwallace@Blairs-MacBook-Pro.local>
2020-05-22 11:27:02 -0500

    adding println

--- src/main/java/com/test/git/project/demo/DemoApplication.java
+++ src/main/java/com/test/git/project/demo/DemoApplication.java
@@ -8,6 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		System.out.println("\n Hello world! \nThis is a test projetc that has a typo.");
 	}
 
 }
